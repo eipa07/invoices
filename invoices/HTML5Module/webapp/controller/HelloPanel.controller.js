1a1fa991f4acb14ad5ab7ab82d5982f293c13408
eipa07 <erick.pina007@gmail.com>
2021-02-16 05:24:49 +0000

    7.3 Dialogos Definicion ok

--- invoices/HTML5Module/webapp/controller/HelloPanel.controller.js
+++ invoices/HTML5Module/webapp/controller/HelloPanel.controller.js
@@ -1,17 +1,18 @@
 // @ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
          * 
          * @param {typeof sap.ui.core.mvc.Controller}           Controller
          * @param {typeof sap.m..MessageToast}                  MessageToast
-         * 
+         * @param {typeof sap.ui.core.Fragment} Fragment
          * 
          */
 
-    (Controller, MessageToast) => {
+    (Controller, MessageToast, Fragment) => {
         "use strict";
 
         return Controller.extend("com.ui5.invoices.controller.HelloPanel", {
@@ -27,6 +28,23 @@ sap.ui.define([
                 var sMsg = oBundle.getText("Mensage", [sRecipient]);
 
                 MessageToast.show(sMsg);
+            },
+
+            onOpenDialog: function (){
+                const oView = this.getView();
+
+                if (!this.byId("helloDialog")) {
+                    Fragment.load({
+                    id: oView.getId(),
+                    name: "com.ui5.invoices.view.HelloDialog"
+                }).then(function (oDialog){
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                });
+                } else {
+                    this.byId("helloDialog").open();
+                }
+                
             }
 
         });
