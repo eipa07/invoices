e23ea181844a0a75f49c917a5b6f06372fa2c6a6
eipa07 <erick.pina007@gmail.com>
2021-02-16 04:13:23 +0000

    Inicio ok

---
+++ invoices/HTML5Module/webapp/controller/Main.controller.js
@@ -0,0 +1,22 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+         * 
+         * @param {typeof sap.ui.core.mvc.Controller}           Controller
+         * 
+         */
+
+    (Controller) => {
+        "use strict";
+
+        return Controller.extend("com.ui5.invoices.controller.Main", {
+
+            onInit: () => {
+
+            }
+
+        });
+
+    });
---
+++ invoices/HTML5Module/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/ui5/HTML5Module/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
